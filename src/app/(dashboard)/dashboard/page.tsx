815b18f92acd5389dd643ee52032d226ecceb8e9
Abir Dutta <abirdutta90990@gmail.com>
2025-05-14 23:03:45 +0530

    add: initialised basic project

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1 className="text-3xl font-bold mb-4">Welcome to Dashboard</h1>
+		</div>
+	);
+}
