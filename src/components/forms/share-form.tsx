bb02f628b3c444387022d7d986e19330c6990e63
Abir Dutta <abirdutta90990@gmail.com>
2025-06-09 15:40:57 +0530

    style: update tab navigation UI with simplified padding and separator

--- src/components/forms/share-form.tsx
+++ src/components/forms/share-form.tsx
@@ -9,6 +9,7 @@ import QADisplay from "./qa-display";
 import ShareComponent from "./share-component";
 import { FormSettings } from "./form-settings";
 import { Submissions } from "./submissions";
+import { Separator } from "../ui/separator";
 
 export default function ShareForm({ show }: { show: boolean }) {
 	const pathname = usePathname();
@@ -33,53 +34,51 @@ export default function ShareForm({ show }: { show: boolean }) {
 		<div className={`flex flex-col min-h-[calc(100vh-40px)] pt-10 `}>
 			<Tabs
 				// defaultValue="summary"
-				className="flex flex-col flex-1 w-full h-full"
+				className="flex flex-col flex-1 w-full h-full "
 				onValueChange={setValue}
 				value={value}
 			>
-				<TabsList className="w-full flex border-b bg-transparent p-0">
+				<TabsList className="w-full flex  bg-transparent p-0 m-0 max-w-lg ">
 					<TabsTrigger
 						value="summary"
-						className="flex-1 text-sm font-medium px-4 py-2 bg-white rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black  "
+						className="  flex-1 text-sm font-medium px-0 py-2 rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=active]:shadow-none "
 					>
 						Summary
 					</TabsTrigger>
 					<TabsTrigger
 						value="submissions"
-						className="flex-1 text-sm font-medium px-4 py-2 bg-transparent rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=inactive]:text-gray-500 focus:outline-none"
+						className="  flex-1 text-sm font-medium px-0 py-2 rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=active]:shadow-none "
 					>
 						Submissions
 					</TabsTrigger>
 					<TabsTrigger
 						value="share"
-						className="flex-1 text-sm font-medium px-4 py-2 bg-transparent rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=inactive]:text-gray-500 focus:outline-none"
+						className="  flex-1 text-sm font-medium px-0 py-2 rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=active]:shadow-none "
 					>
 						Share
 					</TabsTrigger>
 					<TabsTrigger
 						value="integrations"
-						className="flex-1 text-sm font-medium px-4 py-2 bg-transparent rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=inactive]:text-gray-500 focus:outline-none"
+						className="  flex-1 text-sm font-medium px-0 py-2 rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=active]:shadow-none "
 					>
 						Integrations
 					</TabsTrigger>
 					<TabsTrigger
 						value="settings"
-						className="flex-1 text-sm font-medium px-4 py-2 bg-transparent rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=inactive]:text-gray-500 focus:outline-none"
+						className="  flex-1 text-sm font-medium px-0 py-2 rounded-none border-b-2 border-transparent data-[state=active]:border-black data-[state=active]:font-bold data-[state=active]:text-black data-[state=active]:shadow-none "
 					>
 						Settings
 					</TabsTrigger>
 				</TabsList>
-
+				<Separator />
 				<TabsContent value="share" className="mt-8">
 					<ShareComponent
 						placeholderUrl={`${url}/r/${pathname.split("/").slice(-2, -1)[0]}`}
 					/>
 				</TabsContent>
-
 				<TabsContent value="integrations" className="mt-8">
 					<Integrations />
 				</TabsContent>
-
 				<TabsContent value="submissions" className="flex flex-col flex-1 ">
 					<Submissions
 						submissions={submissionData?.data}
@@ -87,7 +86,6 @@ export default function ShareForm({ show }: { show: boolean }) {
 						changeTab={setValue}
 					/>
 				</TabsContent>
-
 				<TabsContent value="summary" className="flex flex-col flex-1">
 					<QADisplay
 						data={submissionData?.data}
@@ -96,7 +94,6 @@ export default function ShareForm({ show }: { show: boolean }) {
 						className="flex-1 flex items-center justify-center"
 					/>
 				</TabsContent>
-
 				<TabsContent value="settings" className="mt-8">
 					<FormSettings />
 				</TabsContent>
